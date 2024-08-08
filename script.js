b45e52104dee91fd12493305ac318b78c5f2d57f
mrharshvashist <mrharshvashist@gmail.com>
2024-08-08 17:12:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// var bcc= mobile.style.backgroundColor;
+
+// var mobile = document.getElementById("mobile");
+var x = document.getElementById("mobile");
+var camera = document.getElementById("camera");
+var speaker = document.getElementById("speaker");
+var y = document.getElementById("img");
+function Xchange() {
+    x.classList.toggle("toggle");
+    y.classList.toggle("click");
+    camera.classList.toggle("blink");
+
+        // x.style.backgroundColor = "blue";
+        // camera.style.backgroundColor = "white";
+        // speaker.style.boxShadow = "0 0 200px red";
+// if (x.classList.contains("toggle")) {
+// }
+
+// // else if(x.style.backgroundColor=="blue"){
+    
+// //     x.style.boxShadow = "0 0 200 blue";}
+// else{
+//     x.style.backgroundColor = "none";
+    
+// }
+
+}
